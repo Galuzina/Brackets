c4656fce290e54512c7e1825393e13a7d6eee4ad
Galuzina <johndoe@example.com>
2018-11-11 23:46:46 +0300

    JS доделай

--- WD1/sc.js
+++ WD1/sc.js
@@ -1,9 +1,15 @@
 menu.onclick = function MyFunction(){
     var x = document.getElementById("Mytopnav");
+    var y = document.getElementById("Mygalary");
     if (x.className === "topnav"){
         x.className+=" responsive";
     } else {
         x.className="topnav";
+    }
+    if (y.className === "pict"){
+        y.className+=" active";
+    } else {
+        y.className="pict";
     }
     
     }
