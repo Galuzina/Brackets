c5b57569a3fc04eee380f50e766d3a86c9bd474e
Galuzina <johndoe@example.com>
2018-11-12 12:42:54 +0300

    projects

--- WD1/sc.js
+++ WD1/sc.js
@@ -1,15 +1,8 @@
-menu.onclick = function MyFunction(){
+menu.onclick = function bar(){
     var x = document.getElementById("Mytopnav");
-    var y = document.getElementById("Mygalary");
     if (x.className === "topnav"){
         x.className+=" responsive";
     } else {
         x.className="topnav";
     }
-    if (y.className === "pict"){
-        y.className+=" active";
-    } else {
-        y.className="pict";
-    }
-    
-    }
+}
